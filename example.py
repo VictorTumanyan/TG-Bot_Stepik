cf26765f0069d4aefd3ed997396098c2630c3dbd
Viktor Tumanian <victumanyan@gmail.com>
2023-10-24 02:42:59 +0400

    wrote the cat-bot

---
+++ example.py
@@ -0,0 +1,43 @@
+# URL = https://api.telegram.org/bot6737278711:AAEd7eFofaoj5n5n69lqBvb71RrghrS-xkE/
+# chat_id= 6325704944
+
+
+import requests # type:ignore
+import time
+
+API_URL = 'http://api.telegram.org/bot'
+API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
+BOT_TOKEN = '6737278711:AAEd7eFofaoj5n5n69lqBvb71RrghrS-xkE'
+GOAR_LIST = ['Малявка, это ты?)', 'А вот это?', 'Ну тут по-любому ты', 'Ладно, вот ты ахах']
+ERROR_TEXT = 'Здесь должна быть картинка с котиком :('
+TEXT = 'Опачки! Апдейтик'
+MAX_COUNTER = 100
+
+offset = -5
+counter = 0
+chat_id: int
+cat_response: requests.Response
+cat_url: str
+
+requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={6325704944}&text={"Эй, напиши мне"}')
+
+while counter < MAX_COUNTER:
+    print('attempt =', counter)
+
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset+1}').json()   # берёт только последние 4 апдейта
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+
+            cat_response = requests.get(API_CATS_URL)
+
+            if cat_response.status_code == 200:
+                cat_url = cat_response.json()[0]['url']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_url}&caption={GOAR_LIST[counter%4]}')
+            else:
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
+
+            time.sleep(1)
+            counter += 1
--- file.py
+++
@@ -1,3 +0,0 @@
-def say_something(number: int, word: str) -> str:
-    word = word.capitalize()
-    return word
