67448ddc4a0b2f308d5bb7dbe257f0e604db337b
Viktor Tumanian <victumanyan@gmail.com>
2023-10-26 20:54:30 +0400

    merged echo and cat bots

--- Bots/echo_bot.py
+++ Bots/echo_bot.py
@@ -5,8 +5,13 @@ from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
 
+import requests
+
 API_URL = 'http://api.telegram.org/bot'
 BOT_TOKEN = '6737278711:AAEd7eFofaoj5n5n69lqBvb71RrghrS-xkE'
+
+CHAT_ID = 6325704944
+API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
@@ -18,10 +23,24 @@ async def process_start_command(message: Message):
 @dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer(
-        'Что тебе помочь то?'
+        'Что тебе помочь то?\n'
         'Просто пишешь сообщение - я тебе отправляю его же'
     )
+
+@dp.message(Command(commands=['cat']))
+async def process_cat_command(message: Message):
+    cat_response = requests.get(API_CATS_URL)
+
+    if cat_response.status_code == 200:
+        cat_url = cat_response.json()[0]['url']
+        requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={CHAT_ID}&photo={cat_url}&caption={"I am a cute cat :)"}')
+    else:
+        requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&text={"Кажется, где-то произошла ошибочка"}')
 
 @dp.message()
 async def send_echo(message: Message):
     await message.reply(text=message.text)
+
+
+if __name__ == "__main__":
+    dp.run_polling(bot)
