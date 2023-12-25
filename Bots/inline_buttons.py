1ffbcd57e12420190641bcd14374b2c653927de4
Viktor Tumanian <victumanyan@gmail.com>
2023-12-25 12:09:53 +0400

    learned about inline buttons

--- Bots/inline_buttons.py
+++ Bots/inline_buttons.py
@@ -1,5 +1,5 @@
-from aiogram import Bot, Dispatcher
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
 from aiogram.filters import CommandStart
 
 from environs import Env
@@ -12,27 +12,45 @@ bot_token = env('BOT_TOKEN')
 bot = Bot(token=bot_token)
 dp = Dispatcher()
 
-url_button1 = InlineKeyboardButton(
-    text='Курс "Телеграм-боты на Python и AIOgram"',
-    url='https://stepik.org/120924'
+big_button1 = InlineKeyboardButton(
+    text='Большая кнопка 1',
+    callback_data='big_button1_pressed'
 )
 
-url_button2 = InlineKeyboardButton(
-    text='Документация Telegram Bot API',
-    url='https://core.telegram.org/bots/api'
+big_button2 = InlineKeyboardButton(
+    text='Большая кнопка 2',
+    callback_data='big_button2_pressed'
 )
 
 keyboard = InlineKeyboardMarkup(
-    inline_keyboard=[[url_button1],
-                     [url_button2]]
+    inline_keyboard=[[big_button1],
+                     [big_button2]]
 )
 
 @dp.message(CommandStart())
 async def process_start_command(message: Message):
     await message.answer(
-        text='Это инлайн-кнопки с параметром "url"',
+        text='Это инлайн-кнопки с параметром "callback"',
         reply_markup=keyboard
     )
+
+@dp.callback_query(F.data == 'big_button1_pressed')
+async def process_button1_pressed(callback: CallbackQuery):
+    if callback.message.text != 'Была нажата кнопка "Большая кнопка 1"':
+        await callback.message.edit_text(
+            text='Была нажата кнопка "Большая кнопка 1"',
+            reply_markup=callback.message.reply_markup
+        )
+    await callback.answer()
+
+@dp.callback_query(F.data == 'big_button2_pressed')
+async def process_button2_pressed(callback: CallbackQuery):
+    if callback.message.text != 'Была нажата кнопка "Большая кнопка 2"':
+        await callback.message.edit_text(
+            text='Была нажата кнопка "Большая кнопка 2"',
+            reply_markup=callback.message.reply_markup
+        )
+    await callback.answer()
 
 if __name__ == '__main__':
     dp.run_polling(bot)
